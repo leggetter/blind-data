6f4d9fd8ed52582d26637a5e093b044dd13aa13b
Phil Leggetter <phil@leggetter.co.uk>
2013-07-14 00:48:26 +0100

    very basic search results & easter egg

--- chscot2013-linkeddata.js
+++ chscot2013-linkeddata.js
@@ -2,9 +2,29 @@ Searches = new Meteor.Collection('Searches');
 
 if (Meteor.isClient) {
 
+  var sound;
+  soundManager.setup( {
+    preferFlash: false,
+    onready: function() {
+
+      sound = soundManager.createSound({
+        id: 'mySound',
+        url: 'assets/media/blind-date.mp3',
+        autoLoad: true,
+        autoPlay: false,
+        onload: function() {
+        }
+      });
+    }
+  } );
+
   Handlebars.registerHelper('userLoggedIn', function () {
     var user = Meteor.user();
     return (!_.isUndefined(user) && !_.isNull(user));
+  });
+
+  Handlebars.registerHelper('highlightJS', function ( data ) {
+    return Prism.highlight( data, Prism.languages.javascript );
   });
 
   Template.search.events({
@@ -14,6 +34,12 @@ if (Meteor.isClient) {
       if( searchStr ) {
 
         var search = { text: searchStr };
+
+        if( search.text.toLowerCase() === 'cilla' ) {
+          sound.play();
+          return;
+        }
+
         var exists = Searches.findOne( search );
         if( !exists ) {
           search.count = 1;
@@ -32,7 +58,12 @@ if (Meteor.isClient) {
         var searchService = new ChScotSearch();
         searchService.search( search.text, {
           done: function( data ) {
-            console.log( 'done', data );
+            var items = [];
+            for( var datasetName in data ) {
+              items = normalize( datasetName, data[ datasetName ] );
+              items = items.concat( data[ datasetName ] );
+            }
+            Session.set( 'search-results', items );
           },
           fail: function() {
             console.log( arguments );
@@ -47,9 +78,28 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.search.searchResults = function() {
+    return Session.get( 'search-results' );
+  }
+
   Template.activeSearches.searches = function() {
     return Searches.find({}).fetch();
   };
+
+  /**
+   * create some common field names to show in the results view
+   */
+  function normalize( dsName, items ) {
+    var item,
+        json;
+    for( var i = 0, l = items.length; i < l; ++i ) {
+      item = items[ i ];
+      json = JSON.stringify( item, undefined, 2 );
+      item.__json = json;
+      item.__dsName = dsName;
+    }
+    return items;
+  }
 }
 
 if (Meteor.isServer) {
